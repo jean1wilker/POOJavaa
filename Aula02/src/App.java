8f6ddddf2dc600e5a2a3444066698090ba5ac335
jean wilker <jean.wilker.sm@gmail.com>
2022-07-15 17:30:16 -0300

    aula 3
    To usando as aulas no mesmo código

--- Aula02/src/App.java
+++ Aula02/src/App.java
@@ -3,22 +3,13 @@ package Aula02.src;
 public class App {
     public static void main(String[] args) throws Exception {
         Caneta c1 = new Caneta();
+        c1.modelo = "BIC Cristal";
         c1.cor = "azul";
-        c1.ponta = 0.5f;
-        c1.modelo = "Bic";
-        c1.carga = 10;
-        c1.destampar();
-        c1.rabiscar();
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
+        //c1.tampada = false;
         c1.status();
-        System.out.println("\n");
-        Caneta c2 = new Caneta();
-        c2.cor = "preta";
-        c2.ponta = 0.7f;
-        c2.modelo = "bools";
-        c2.carga = 50;
 
-        c2.tampar();
-        c2.rabiscar();
-        c2.status();
+        c1.rabiscar();
     }
 }
--- Aula02/src/Caneta.java
+++ Aula02/src/Caneta.java
@@ -3,20 +3,20 @@ package Aula02.src;
 import java.io.ObjectInputValidation;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
     
-    void status(){
+    public void status(){
         System.out.println("Qual o modelo?" + this.modelo);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga + "%");
         System.out.println("A cor da caneta é " + this.cor);
         System.out.println("Está tampada? " + this.tampada);
     }
-    void rabiscar(){
+    public void rabiscar(){
         if(this.tampada == true){
         System.out.println("Error! Nao posso rabiscar, tente retirar a tampa.");
         }else{
@@ -24,11 +24,11 @@ public class Caneta {
         }
     }
 
-    void tampar(){
+    public void tampar(){
         this.tampada = true;
     }
 
-    void destampar(){
+    public void destampar(){
         this.tampada = false;
     }
 }
